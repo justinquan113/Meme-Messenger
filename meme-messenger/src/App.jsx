9b97106120350b425f0fa1f83251b0cb69ef52e8
Justin Quan <jquan141@mtroyal.ca>
2025-08-27 00:27:11 -0600

    created database and insert method

--- meme-messenger/src/App.jsx
+++ meme-messenger/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     })
     .then(data =>{
         let message = data.message
-        console.log(message)
+        
         setMessage(message)
       })
     .catch(error => {
@@ -26,7 +26,17 @@ function App() {
     
     }
   
+  function handleClick(){
+    fetch('http://127.0.0.1:5000/show')
+    .then(res => {
+      return res.json()
+    })
+    .then(data =>{
+      console.log(data.registered_numbers)
+    })
+  }
 
+  
   return (
     <div className='flex justify-center items-center h-screen bg-green-400' onSubmit={handleSubmit} >
       <form className='flex justify-center w-1/3 h-1/4 bg-gray-100 rounded-md'>
@@ -40,14 +50,18 @@ function App() {
               type='tel'
               required
               value={phoneNumber}
-              
+              pattern='[1-9]{1}[0-9]{9}'
+              onInvalid={(e) => e.target.setCustomValidity('Please enter a valid phone number')}
+              onInput={(e) => e.target.setCustomValidity('')}
             />
           </div>
           <button className='w-3/4 bg-blue-300 rounded-md self-center cursor-pointer' type='submit' >Enter</button>
+          <p className='self-center'>{message}</p>
+          
         </div>
-        <p>{message}</p>
+       
       </form>
-      
+      <button onClick={handleClick}>GET ALL</button>
     </div>
   )
 }
--- server/meme.py
+++ server/meme.py
@@ -1,12 +1,69 @@
 from flask import Flask
 from flask_cors import CORS
+import sqlite3
+from twilio.rest import Client
 app = Flask(__name__)
 CORS(app)
+
+connection = sqlite3.connect('store_phoneNumbers.db')
+
+#interact with database
+cursor = connection.cursor()
+
+command1 = """CREATE TABLE IF NOT EXISTS
+numbers(number INTEGER PRIMARY KEY)"""
+
+cursor.execute(command1)
+connection.commit()
+
+
+
+
+connection.close()
+
 @app.route('/submit/<phoneNumber>')
 def submit(phoneNumber):
+    connection = sqlite3.connect('store_phoneNumbers.db')
+    cursor = connection.cursor()
+    try:
+        cursor.execute("INSERT INTO numbers  VALUES ({phoneNumber})".format(phoneNumber = phoneNumber))
+        connection.commit()
+        message = f"You will now receive daily memes at {phoneNumber}"
+    except sqlite3.IntegrityError:
+        message = f"{phoneNumber} is already registered."
    
-    return {'message': f"You will now receive daily memes at {phoneNumber}"}
+    
+    finally:
+        connection.close()
+        
+    return {'message': message}
+
+
+
+@app.route('/show')
+def showNumbers():
+    connection = sqlite3.connect('store_phoneNumbers.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM numbers')
+    results = cursor.fetchall()
+    connection.close()
+    
+    return {'registered_numbers' : results}
+    
+
+
+connection = sqlite3.connect('store_phoneNumbers.db')
+cursor = connection.cursor()
+cursor.execute('SELECT * FROM numbers')
+result = cursor.fetchall()
+for row in result:
+    number = row[0]
+    
+connection.close()
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    
+    
