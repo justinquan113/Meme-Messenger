219bbaf2403f1088dcfb98baa035746a1a96fa42
Justin Quan <jquan141@mtroyal.ca>
2026-02-06 15:24:05 -0700

    changes

--- server/app.py
+++ server/app.py
@@ -145,7 +145,8 @@ def health_check():
     return "OK", 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
     
 
     
